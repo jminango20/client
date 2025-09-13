f87f76bbdc656b9f0b5be16f9718a1930903f73c
Juan Minango <juan.minango@bbchain.com.br>
2025-09-12 23:40:14 -0300

    feat: retrieves Binance websocket

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const WebSocket = require('ws');
+
+const ws = new WebSocket('wss://fstream.binance.com/ws/bnbusdt@aggTrade');
+
+ws.onmessage = (event) => {
+    const obj = JSON.parse(event.data);
+    console.log(`Symbol: ${obj.s}, Bid: ${obj.b}, Ask: ${obj.a}`);
+}
