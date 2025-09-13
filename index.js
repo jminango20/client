b1c256dc05b27c5b25bed3b6b227a53c62952324
Juan Minango <juan.minango@bbchain.com.br>
2025-09-12 23:46:10 -0300

    feat: message via websocket

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const WebSocket = require('ws');
+const readlineSync = require('readline-sync');
 
 const ws = new WebSocket('wss://fstream.binance.com/ws/bnbusdt@aggTrade');
+
+const symbol = readlineSync.question('Enter the symbol: ');
+
+ws.on('open', () => {
+    ws.send(JSON.stringify({
+        "method": "SUBSCRIBE",
+        "params": [`${symbol.toLowerCase()}@aggTrade`],
+        "id": 1
+    }));
+})
 
 ws.onmessage = (event) => {
     const obj = JSON.parse(event.data);
